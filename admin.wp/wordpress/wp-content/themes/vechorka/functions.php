a12bdcfffa2d14df7c6928b229ac0b615c656850
alxpgv <alex@ipcoder.ru>
2022-09-25 12:23:14 +0300

    upd

---
+++ admin.wp/wordpress/wp-content/themes/vechorka/functions.php
@@ -0,0 +1,7 @@
+<?php
+$func_dir = get_template_directory() . '/functions';
+
+require_once $func_dir . '/functions.php';
+require_once $func_dir . '/setup.php';
+require_once $func_dir . '/post-types.php';
+require_once $func_dir . '/acf.php';
---
+++ admin.wp/wordpress/wp-content/themes/vechorka/functions/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+function redirect_404()
+{
+  global $wp_query;
+  $wp_query->set_404();
+  status_header(404);
+  get_template_part(404);
+  exit();
+}
