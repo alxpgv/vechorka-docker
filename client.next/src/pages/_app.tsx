238061f1cfbf991f4d8413a7ad7c0e823142cc17
alxpgv <alex@ipcoder.ru>
2022-10-03 02:55:44 +0300

    disable progress

--- client.next/src/pages/_app.tsx
+++ client.next/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import NextNProgress from "nextjs-progressbar";
+// import NextNProgress from "nextjs-progressbar";
 import { SettingsProvider } from "@/contexts/settings-context";
 import { Head } from "@/components/common/head";
 import { Layout } from "@/components/common/layouts";
@@ -9,11 +9,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   const { settings, taxonomies } = pageProps;
   return (
     <>
-      <NextNProgress
-        color="#154291"
-        height={2}
-        options={{ showSpinner: false }}
-      />
+      {/*<NextNProgress*/}
+      {/*  color="#154291"*/}
+      {/*  height={2}*/}
+      {/*  options={{ showSpinner: false }}*/}
+      {/*/>*/}
       <SettingsProvider value={{ ...settings, taxonomies }}>
         <Head />
         <Layout>
