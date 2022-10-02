277f28d5418b4e7a59270ae8ba01cbfa574d8b58
alxpgv <alex@ipcoder.ru>
2022-10-02 23:55:24 +0300

    change fonts loading

--- client.next/src/components/common/head/head.tsx
+++ client.next/src/components/common/head/head.tsx
@@ -8,30 +8,30 @@ export const Head = () => {
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         key="viewport"
       />
-      <link
-        rel="preload"
-        href="/fonts/raleway/raleway-v28-latin_cyrillic-regular.woff2"
-        as="font"
-        type="font/woff2"
-        crossOrigin=""
-        key="ralewayRegular"
-      />
-      <link
-        rel="preload"
-        href="/fonts/raleway/raleway-v28-latin_cyrillic-700.woff2"
-        as="font"
-        type="font/woff2"
-        crossOrigin=""
-        key="ralewayBold"
-      />
-      <link
-        rel="preload"
-        href="/fonts/roboto/roboto-v30-latin_cyrillic-regular.woff2"
-        as="font"
-        type="font/woff2"
-        crossOrigin=""
-        key="robotoRegular"
-      />
+      {/*<link*/}
+      {/*  rel="preload"*/}
+      {/*  href="/fonts/raleway/raleway-v28-latin_cyrillic-regular.woff2"*/}
+      {/*  as="font"*/}
+      {/*  type="font/woff2"*/}
+      {/*  crossOrigin=""*/}
+      {/*  key="ralewayRegular"*/}
+      {/*/>*/}
+      {/*<link*/}
+      {/*  rel="preload"*/}
+      {/*  href="/fonts/raleway/raleway-v28-latin_cyrillic-700.woff2"*/}
+      {/*  as="font"*/}
+      {/*  type="font/woff2"*/}
+      {/*  crossOrigin=""*/}
+      {/*  key="ralewayBold"*/}
+      {/*/>*/}
+      {/*<link*/}
+      {/*  rel="preload"*/}
+      {/*  href="/fonts/roboto/roboto-v30-latin_cyrillic-regular.woff2"*/}
+      {/*  as="font"*/}
+      {/*  type="font/woff2"*/}
+      {/*  crossOrigin=""*/}
+      {/*  key="robotoRegular"*/}
+      {/*/>*/}
     </SEO>
   );
 };
