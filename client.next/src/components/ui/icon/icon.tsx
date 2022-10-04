36c3a02d23bce96a48087c215b1630e2cefe1c24
alxpgv <alex@ipcoder.ru>
2022-10-04 14:59:49 +0300

    upd

--- client.next/src/components/ui/icon/icon.tsx
+++ client.next/src/components/ui/icon/icon.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from "react";
+import React from "react";
 
 interface IconProps {
   name: string;
   className?: string;
 }
 
-export const Icon: FC<IconProps> = ({ name, className }) => {
+export const Icon = ({ name, className }: IconProps) => {
   return (
     <svg className={className}>
       <use xlinkHref={`/sprites/icon.svg#${name.toLocaleLowerCase()}`} />
