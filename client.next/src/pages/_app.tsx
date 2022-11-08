97f8ebb237fd2997f88716fcbd3ae4bc3f945696
alxpgv <alex@ipcoder.ru>
2022-11-09 02:04:37 +0300

    add progressbar

--- client.next/src/pages/_app.tsx
+++ client.next/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/app/styles/globals.css";
 import type { AppProps } from "next/app";
-// import NextNProgress from "nextjs-progressbar";
+import NextNProgress from "nextjs-progressbar";
 import { SettingsProvider } from "@/app/contexts/settings-context";
 import { Head } from "@/shared/ui/head";
 import { Layout } from "@/shared/ui/layouts";
@@ -9,11 +9,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   const { settings, taxonomies, advert } = pageProps;
   return (
     <>
-      {/*<NextNProgress*/}
-      {/*  color="#154291"*/}
-      {/*  height={2}*/}
-      {/*  options={{ showSpinner: false }}*/}
-      {/*/>*/}
+      <NextNProgress
+        color="#154291"
+        height={2}
+        options={{ showSpinner: false }}
+      />
       <SettingsProvider value={{ ...settings, taxonomies, advert }}>
         <Head />
         <Layout>
