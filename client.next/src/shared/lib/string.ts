957a040298a30b7cf0c2955221b19e6ac7b4e378
alxpgv <alex@ipcoder.ru>
2022-11-08 20:26:48 +0300

    add text-overflow function

--- client.next/src/shared/lib/string.ts
+++ client.next/src/shared/lib/string.ts
@@ -2,7 +2,7 @@ export const getPhoneFromString = (value: string) => {
   return value ? value.replace(/[^0-9+]/g, "") : "";
 };
 
-// export function textOverflow(term: string, limit: number = 100) {
-//   if (term.length <= limit) return term;
-//   return `${term.slice(0, limit)}...`;
-// }
+export function textOverflow(term: string, limit = 100) {
+  if (term.length <= limit) return term;
+  return `${term.slice(0, limit)}...`;
+}
