780b325d1c136589e404160f981ad9b088c4ab52
alxpgv <alex@ipcoder.ru>
2022-11-16 21:31:34 +0300

    strip text

--- client.next/src/shared/lib/string.ts
+++ client.next/src/shared/lib/string.ts
@@ -2,7 +2,21 @@ export const getPhoneFromString = (value: string) => {
   return value ? value.replace(/[^0-9+]/g, "") : "";
 };
 
-export function textOverflow(term: string, limit = 100) {
+export const textOverflow = (term: string, limit = 100) => {
   if (term.length <= limit) return term;
   return `${term.slice(0, limit)}...`;
-}
+};
+
+export const stripText = (text: string) => {
+  text = text.replace(/(<([^>]+)>)/gim, "");
+  text = text.replace(/\[.+\]/gi, "");
+  text = text.replace(/\t|\s\t/gi, " ");
+  text = text.replace(/\r\n/gi, " ");
+  text = text.replace(/&nbsp;/gi, " ");
+  text = text.replace(/&/gi, "&amp;");
+  text = text.replace(/"/gi, "&quot;");
+  text = text.replace(/'/gi, "&apos;");
+  text = text.replace(/>/gi, "&gt;");
+  text = text.replace(/</gi, "&lt;");
+  return text;
+};
