a73346984995665595e092a2fc6236b3eb552297
alxpgv <alex@ipcoder.ru>
2022-12-02 02:01:14 +0300

    show advert, show paper

--- client.next/src/shared/ui/layouts/post-layout.tsx
+++ client.next/src/shared/ui/layouts/post-layout.tsx
@@ -11,14 +11,18 @@ import { useSettings } from "@/app/contexts/settings-context";
 
 interface Props {
   left: React.ReactNode;
-  showNewsWidgets?: boolean;
   interestPosts?: PostProps[];
+  showNewsWidgets?: boolean;
+  showAdvert?: boolean;
+  showPaper?: boolean;
 }
 
 export const PostLayout: FC<Props> = ({
   left,
   interestPosts,
   showNewsWidgets = true,
+  showAdvert = true,
+  showPaper = true,
 }) => {
   const { advert } = useSettings();
   return (
@@ -31,28 +35,32 @@ export const PostLayout: FC<Props> = ({
               <NavCategories />
             </div>
             {/* advert block 2 mobile 300x300 */}
-            {advert && !!Number(advert.advert_block_2_mobile_visible) && (
-              <DynamicAdvert
-                className="block md:hidden my-6"
-                type={advert.advert_block_2_mobile_type}
-                size="300x300"
-                imageUrl={advert.advert_block_2_mobile_image_url}
-                href={advert.advert_block_2_mobile_href}
-                htmlCode={advert.advert_block_2_mobile_html_code}
-              />
-            )}
+            {showAdvert &&
+              advert &&
+              !!Number(advert.advert_block_2_mobile_visible) && (
+                <DynamicAdvert
+                  className="block md:hidden my-6"
+                  type={advert.advert_block_2_mobile_type}
+                  size="300x300"
+                  imageUrl={advert.advert_block_2_mobile_image_url}
+                  href={advert.advert_block_2_mobile_href}
+                  htmlCode={advert.advert_block_2_mobile_html_code}
+                />
+              )}
 
             {/* advert block 2 pc 300x600 */}
-            {advert && !!Number(advert.advert_block_2_pc_visible) && (
-              <DynamicAdvert
-                className="hidden md:block mt-6"
-                type={advert.advert_block_2_pc_type}
-                size="300x600"
-                imageUrl={advert.advert_block_2_pc_image_url}
-                href={advert.advert_block_2_pc_href}
-                htmlCode={advert.advert_block_2_pc_html_code}
-              />
-            )}
+            {showAdvert &&
+              advert &&
+              !!Number(advert.advert_block_2_pc_visible) && (
+                <DynamicAdvert
+                  className="hidden md:block mt-6"
+                  type={advert.advert_block_2_pc_type}
+                  size="300x600"
+                  imageUrl={advert.advert_block_2_pc_image_url}
+                  href={advert.advert_block_2_pc_href}
+                  htmlCode={advert.advert_block_2_pc_html_code}
+                />
+              )}
             {showNewsWidgets && (
               <div className="smx1:flex md:block mt-6">
                 <NewsTopDynamic className="smx1:mr-3 md:mr-0" />
@@ -61,41 +69,47 @@ export const PostLayout: FC<Props> = ({
             )}
 
             {/* advert block 3 300x300 */}
-            {advert && !!Number(advert.advert_block_3_visible) && (
-              <DynamicAdvert
-                className="mt-6"
-                type={advert.advert_block_3_type}
-                size="300x300"
-                imageUrl={advert.advert_block_3_image_url}
-                href={advert.advert_block_3_href}
-                htmlCode={advert.advert_block_3_html_code}
-              />
-            )}
+            {showAdvert &&
+              advert &&
+              !!Number(advert.advert_block_3_visible) && (
+                <DynamicAdvert
+                  className="mt-6"
+                  type={advert.advert_block_3_type}
+                  size="300x300"
+                  imageUrl={advert.advert_block_3_image_url}
+                  href={advert.advert_block_3_href}
+                  htmlCode={advert.advert_block_3_html_code}
+                />
+              )}
 
-            <PaperLinks className="relative md:mt-20" />
+            {showPaper && <PaperLinks className="relative md:mt-20" />}
 
             {/* advert block 4 pc 300x600 */}
-            {advert && !!Number(advert.advert_block_4_pc_visible) && (
-              <DynamicAdvert
-                className="hidden md:block mt-6"
-                type={advert.advert_block_4_pc_type}
-                size="300x600"
-                imageUrl={advert.advert_block_4_pc_image_url}
-                href={advert.advert_block_4_pc_href}
-                htmlCode={advert.advert_block_4_pc_html_code}
-              />
-            )}
+            {showAdvert &&
+              advert &&
+              !!Number(advert.advert_block_4_pc_visible) && (
+                <DynamicAdvert
+                  className="hidden md:block mt-6"
+                  type={advert.advert_block_4_pc_type}
+                  size="300x600"
+                  imageUrl={advert.advert_block_4_pc_image_url}
+                  href={advert.advert_block_4_pc_href}
+                  htmlCode={advert.advert_block_4_pc_html_code}
+                />
+              )}
             {/* advert block 4 mobile 300x300 */}
-            {advert && !!Number(advert.advert_block_4_mobile_visible) && (
-              <DynamicAdvert
-                className="block md:hidden mt-6"
-                type={advert.advert_block_4_mobile_type}
-                size="300x300"
-                imageUrl={advert.advert_block_4_mobile_image_url}
-                href={advert.advert_block_4_mobile_href}
-                htmlCode={advert.advert_block_4_mobile_html_code}
-              />
-            )}
+            {showAdvert &&
+              advert &&
+              !!Number(advert.advert_block_4_mobile_visible) && (
+                <DynamicAdvert
+                  className="block md:hidden mt-6"
+                  type={advert.advert_block_4_mobile_type}
+                  size="300x300"
+                  imageUrl={advert.advert_block_4_mobile_image_url}
+                  href={advert.advert_block_4_mobile_href}
+                  htmlCode={advert.advert_block_4_mobile_html_code}
+                />
+              )}
           </>
         }
       />
