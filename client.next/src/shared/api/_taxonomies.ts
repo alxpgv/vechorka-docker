f6765fde60693bacdfd5249d4dfc9c009f07368d
alxpgv <alex@ipcoder.ru>
2022-12-04 07:11:32 +0300

    remove

--- client.next/src/shared/api/_taxonomies.ts
+++
@@ -1,6 +0,0 @@
-import { TaxonomiesProps } from "@/shared/types";
-import { api } from "@/shared/api/core";
-
-export const getTaxonomiesGroup = (isSSG = false): Promise<TaxonomiesProps> => {
-  return api.get("taxonomies/group", isSSG);
-};
