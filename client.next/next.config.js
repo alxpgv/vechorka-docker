a5fa5652bf1d43792dbdf69d9bec99cd8896cbd8
alxpgv <alex@ipcoder.ru>
2022-12-21 12:29:19 +0300

    add admin.vechorka.ru to images

--- client.next/next.config.js
+++ client.next/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     UPLOAD_HOST: process.env.UPLOAD_HOST,
   },
   images: {
-    domains: ["vechorka.loc", "vechorka.ru", "localhost"],
+    domains: ["vechorka.loc", "vechorka.ru", "admin.vechorka.ru", "localhost"],
     // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [460, 768, 1024, 1200],
